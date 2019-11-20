cdedd37896b78579c9f574b3d6a6d4739409f152
Phung Van Buu <phungvan_buu@yahoo.com.vn>
2019-11-20 13:01:13 +0700

    update news sort & filter principle

--- bot.js
+++ bot.js
@@ -5,12 +5,11 @@ var request = require("request");
 var CronJob = require('cron').CronJob;
 const cheerio = require('cheerio');
 const fs = require('fs');
-var threadList = [];
 const mainChannel = '436013570866806799';
 var holiday_jp = require('@holiday-jp/holiday_jp');
-
 var morningWords = ['Dò', 'Ò hái dó', 'Ồ hái dò'];
-
+var newsList = [];
+var posted = [];
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -36,17 +35,29 @@ function isURL(str) {
     return !!pattern.test(str);
 }
 
-const job = new CronJob('0 */10 10-11 * * 1-5', function() {
-    crawl_data();
-    if (threadList !== undefined && threadList.length != 0) {
-        // array empty or does not exist
-        var thread = threadList.shift();
-        // console.log(thread);
-        bot.channels.get(mainChannel).send(thread);
-    }
+const job = new CronJob('* */10 10-11 * * 1-5', function() {
+    crawl_data().then(function(threadList) {
+        sortNewsList(newsList);
+        var topThread = newsList[0];
+        console.log(topThread.url);
+        newsList.splice(0, 1);
+        posted.push(topThread.id);
+        bot.channels.get(mainChannel).send(topThread.url);
+    }).catch(function(err) {
+        console.error(err.stack);
+    });
+    // if (threadList !== undefined && threadList.length != 0) {
+    //     // array empty or does not exist
+    //     // var thread = threadList.shift();
+    //     // console.log(thread);
+    //     // bot.channels.get(mainChannel).send(thread);
+    // }
 }, null, true, 'Asia/Bangkok');
 
 const morningJob = new CronJob('0 0 9 * * *', function() {
+    newsList = [];
+    posted = [];
+
     var morningWord = morningWords[Math.floor(Math.random() * morningWords.length)];
 
     bot.channels.get(mainChannel).send(morningWord);
@@ -68,6 +79,8 @@ const remindHolidayJob = new CronJob('0/10 * * * * *', function() {
 
 bot.on('ready', function(evt) {
     logger.info('Connected');
+    const ayy = bot.emojis.find(emoji => emoji.name === "pepe_gun_2");
+    bot.channels.get(mainChannel).send(`Tao lại vừa thông minh hơn xíu dòi! ${ayy} `);
     job.start();
     morningJob.start();
     remindHolidayJob.start();
@@ -91,26 +104,45 @@ bot.on('message', function(message) {
 });
 
 function crawl_data() {
-    var customHeaderRequest = request.defaults({
-        headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36' }
-    })
-    customHeaderRequest.get('https://forums.voz.vn/forumdisplay.php?f=33', function(err, resp, body) {
-        var $ = cheerio.load(body);
-        var threads = $('table#threadslist tr');
-        $(threads).each(function(i, thread) {
-            var link = $(thread).find('td:nth-of-type(2) > div > a');
-            var url = $(link).attr('href');
-            var isToday = $(thread).find('td:nth-of-type(3) > div').text().includes('Today');
-            if (url != undefined && isToday && checkShowthread(url)) {
-                logger.info(url);
-                threadList.push("https://forums.voz.vn/" + url);
-            }
+    return new Promise(function(resolve, reject) {
+        var customHeaderRequest = request.defaults({
+            headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36' }
+        })
+        customHeaderRequest.get('https://forums.voz.vn/forumdisplay.php?f=33', function(err, resp, body) {
+            var $ = cheerio.load(body);
+            var threads = $('table#threadslist > tbody#threadbits_forum_33 tr');
+            threads.each(function(i, thread) {
+                var link = $(thread).find('td:nth-of-type(2) > div > a');
+                var title = $(link).text();
+                var url = $(link).attr('href');
+                var id = url.substring(url.indexOf('=') + 1);
+
+                var icon = $(thread).find("td:nth-of-type(2) > div img.inlineimg[src='images/misc/sticky.gif']");
+                var isSticky = $(icon).attr('alt') == 'Sticky Thread';
+
+                var isToday = $(thread).find('td:nth-of-type(3) > div').text().includes('Today');
+                var replyCount = parseInt($(thread).find('td:nth-of-type(4) > a').text().replace(',', ''));
+                var viewCount = parseInt($(thread).find('td:nth-of-type(5)').text().replace(',', ''));
+                var news = { id: id, url: "https://forums.voz.vn/" + url, isToday: isToday, title: title, reply: replyCount, view: viewCount };
+                if (!isSticky && url.toLowerCase().includes('showthread') && isToday) {
+                    if (newsList.map(thread => thread.id).includes(news.id)) {
+                        newsList.splice(newsList.map(thread => thread.id).indexOf(news.id), 1);
+                    }
+                    if (!posted.includes(news.id))
+                        newsList.push(news);
+
+                }
+            });
+            // console.log('chua sort');
+            console.log(newsList.length);
+            resolve(newsList);
         });
     });
 }
 
-function checkShowthread(url) {
-    return url.toLowerCase().includes('showthread');
+function sortNewsList(newsList) {
+    newsList.sort(function(a, b) { return ((b.reply * 0.7 + b.view * 0.3) - (a.reply * 0.7 + a.view * 0.3)) });
 }
+
 
 bot.login(process.env.BOT_TOKEN || auth.token);
