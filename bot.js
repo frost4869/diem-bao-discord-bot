fb608322f911a9ed977f3b8ae6f295ee870245cd
Phung Van Buu <phungvan_buu@yahoo.com.vn>
2019-11-24 13:10:35 +0700

    fix null bot in job

--- bot.js
+++ bot.js
@@ -2,82 +2,84 @@ let Discord = require("discord.js");
 let logger = require("winston");
 let auth = require("./auth.json");
 let request = require("request");
-const jobs = require("./job");
 
 const MAIN_CHANNEL_ID = "436013570866806799";
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
-  colorize: true
+    colorize: true
 });
 logger.level = "debug";
 
 let bot = new Discord.Client();
+
+const jobs = require("./job")(bot);
+
 const botUpdateEmojis = [
-  "pepe_thug",
-  "pepe_gun",
-  "shame",
-  "pepe_up",
-  "pepe_ok",
-  "kappa",
-  "PogChamp",
-  "pepe_gun_2",
-  "pepe_boss"
+    "pepe_thug",
+    "pepe_gun",
+    "shame",
+    "pepe_up",
+    "pepe_ok",
+    "kappa",
+    "PogChamp",
+    "pepe_gun_2",
+    "pepe_boss"
 ];
 
 function rep(message, customMess) {
-  message.delete(1000);
-  message.channel.send(customMess || `<@${message.author.id}> Genk cc nè`);
+    message.delete(1000);
+    message.channel.send(customMess || `<@${message.author.id}> Genk cc nè`);
 }
 
 function isURL(str) {
-  let pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  ); // fragment locator
-  return !!pattern.test(str);
+    let pattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+        "i"
+    ); // fragment locator
+    return !!pattern.test(str);
 }
 
 bot.on("ready", function() {
-  logger.info("Connected");
-  const randomEmo =
-    botUpdateEmojis[Math.floor(Math.random() * botUpdateEmojis.length)];
-  const ayy = bot.emojis.find(emoji => emoji.name === randomEmo);
-  bot.channels
-    .get(MAIN_CHANNEL_ID)
-    .send(`Tao lại vừa thông minh hơn xíu dòi! ${ayy} `);
-  jobs.crawDataJob.start();
-  jobs.morningJob.start();
-  jobs.remindHolidayJob.start();
+    logger.info("Connected");
+    const randomEmo =
+        botUpdateEmojis[Math.floor(Math.random() * botUpdateEmojis.length)];
+    const ayy = bot.emojis.find(emoji => emoji.name === randomEmo);
+    // bot.channels
+    //   .get(MAIN_CHANNEL_ID)
+    //   .send(`Tao lại vừa thông minh hơn xíu dòi! ${ayy} `);
+    jobs.crawDataJob.start();
+    jobs.morningJob.start();
+    jobs.remindHolidayJob.start();
 });
 
 bot.on("message", function(message) {
-  const { content, author } = message;
-  if (content.toLowerCase().includes("genk.vn")) {
-    rep(message);
-  } else {
-    if (isURL(content)) {
-      request({ url: content, followRedirect: false }, function(
-        error,
-        response,
-        body
-      ) {
-        if (response.statusCode >= 300 && response.statusCode < 400) {
-          const realLink = response.headers.location;
-          const isGenkLink = realLink.toLowerCase().includes("genk.vn");
-          if (isGenkLink) {
-            rep(message, `<@${author.id}> Uh có cố gắng, genk clmm`);
-          }
+    const { content, author } = message;
+    if (content.toLowerCase().includes("genk.vn")) {
+        rep(message);
+    } else {
+        if (isURL(content)) {
+            request({ url: content, followRedirect: false }, function(
+                error,
+                response,
+                body
+            ) {
+                if (response.statusCode >= 300 && response.statusCode < 400) {
+                    const realLink = response.headers.location;
+                    const isGenkLink = realLink.toLowerCase().includes("genk.vn");
+                    if (isGenkLink) {
+                        rep(message, `<@${author.id}> Uh có cố gắng, genk clmm`);
+                    }
+                }
+            });
         }
-      });
     }
-  }
 });
 
 bot.login(process.env.BOT_TOKEN || auth.token);
--- job.js
+++ job.js
@@ -8,141 +8,139 @@ const MAIN_CHANNEL_ID = "436013570866806799";
 let newsList = [];
 let posted = [];
 const morningWords = ["Dò", "Ò hái dó", "Ồ hái dò", "o2yo"];
+var myBot;
 
 const remindHolidayJob = new CronJob(
-  "0/10 * * * * *",
-  function() {
-    let today = new Date();
-    let tomorrow = new Date();
-    tomorrow.setDate(today.getDate() + 1);
-    if (tomorrow.getDay() % 6) {
-      let holidays = holiday_jp.between(tomorrow, tomorrow);
-      if (holidays.length != 0) {
-        bot.channels
-          .get(MAIN_CHANNEL_ID)
-          .send(`<@Bủ#1605> Mai là ngày đỏ nha`);
-      }
-    } else {
-      console.log(`<@Bủ#1605> Mai là cúi từng nha`);
-    }
-  },
-  null,
-  true,
-  "Asia/Bangkok"
+    "0 0 16 * * *",
+    function() {
+        let today = new Date();
+        let tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+        if (tomorrow.getDay() % 6) {
+            let holidays = holiday_jp.between(tomorrow, tomorrow);
+            if (holidays.length != 0) {
+                myBot.channels
+                    .get(MAIN_CHANNEL_ID)
+                    .send(`<@Bủ#1605> Mai là ngày đỏ nha`);
+            }
+        } else {
+            console.log(`<@Bủ#1605> Mai là cúi từng nha`);
+        }
+    },
+    null,
+    true,
+    "Asia/Bangkok"
 );
 
 const crawDataJob = new CronJob(
-  "* */10 10-11 * * 1-5",
-  function() {
-    crawl_data()
-      .then(function(threadList) {
-        sortNewsList(newsList);
-        let topThread = newsList[0];
-        newsList.splice(0, 1);
-        posted.push(topThread.id);
-        bot.channels.get(MAIN_CHANNEL_ID).send(topThread.url);
-      })
-      .catch(function(err) {
-        console.error(err.stack);
-      });
-  },
-  null,
-  true,
-  "Asia/Bangkok"
+    "* */10 10-11 * * 1-6",
+    function() {
+        crawl_data()
+            .then(function(threadList) {
+                sortNewsList(newsList);
+                let topThread = newsList[0];
+                newsList.splice(0, 1);
+                posted.push(topThread.id);
+                myBot.channels.get(MAIN_CHANNEL_ID).send(topThread.url);
+            })
+            .catch(function(err) {
+                console.error(err.stack);
+            });
+    },
+    null,
+    true,
+    "Asia/Bangkok"
 );
 
 const morningJob = new CronJob(
-  "0 0 9 * * *",
-  function() {
-    newsList = [];
-    posted = [];
+    "0 0 9 * * *",
+    function() {
+        newsList = [];
+        posted = [];
 
-    let morningWord =
-      morningWords[Math.floor(Math.random() * morningWords.length)];
+        let morningWord =
+            morningWords[Math.floor(Math.random() * morningWords.length)];
 
-    bot.channels.get(MAIN_CHANNEL_ID).send(morningWord);
-  },
-  null,
-  true,
-  "Asia/Bangkok"
+        myBot.channels.get(MAIN_CHANNEL_ID).send(morningWord);
+    },
+    null,
+    true,
+    "Asia/Bangkok"
 );
 
 function crawl_data() {
-  return new Promise(function(resolve, reject) {
-    let customHeaderRequest = request.defaults({
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
-      }
-    });
-    customHeaderRequest.get(
-      "https://forums.voz.vn/forumdisplay.php?f=33",
-      function(err, resp, body) {
-        let $ = cheerio.load(body);
-        let threads = $("table#threadslist > tbody#threadbits_forum_33 tr");
-        threads.each(function(i, thread) {
-          let link = $(thread).find("td:nth-of-type(2) > div > a");
-          let title = $(link).text();
-          let url = $(link).attr("href");
-          let id = url.substring(url.indexOf("=") + 1);
-
-          let icon = $(thread).find(
-            "td:nth-of-type(2) > div img.inlineimg[src='images/misc/sticky.gif']"
-          );
-          let isSticky = $(icon).attr("alt") == "Sticky Thread";
-
-          let isToday = $(thread)
-            .find("td:nth-of-type(3) > div")
-            .text()
-            .includes("Today");
-          let replyCount = parseInt(
-            $(thread)
-              .find("td:nth-of-type(4) > a")
-              .text()
-              .replace(",", "")
-          );
-          let viewCount = parseInt(
-            $(thread)
-              .find("td:nth-of-type(5)")
-              .text()
-              .replace(",", "")
-          );
-          let news = {
-            id: id,
-            url: "https://forums.voz.vn/" + url,
-            isToday: isToday,
-            title: title,
-            reply: replyCount,
-            view: viewCount
-          };
-          if (
-            !isSticky &&
-            url.toLowerCase().includes("showthread") &&
-            isToday
-          ) {
-            if (newsList.map(thread => thread.id).includes(news.id)) {
-              newsList.splice(
-                newsList.map(thread => thread.id).indexOf(news.id),
-                1
-              );
+    return new Promise(function(resolve, reject) {
+        let customHeaderRequest = request.defaults({
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
             }
-            if (!posted.includes(news.id)) newsList.push(news);
-          }
         });
-        resolve(newsList);
-      }
-    );
-  });
+        customHeaderRequest.get(
+            "https://forums.voz.vn/forumdisplay.php?f=33",
+            function(err, resp, body) {
+                let $ = cheerio.load(body);
+                let threads = $("table#threadslist > tbody#threadbits_forum_33 tr");
+                threads.each(function(i, thread) {
+                    let link = $(thread).find("td:nth-of-type(2) > div > a");
+                    let title = $(link).text();
+                    let url = $(link).attr("href");
+                    let id = url.substring(url.indexOf("=") + 1);
+
+                    let icon = $(thread).find(
+                        "td:nth-of-type(2) > div img.inlineimg[src='images/misc/sticky.gif']"
+                    );
+                    let isSticky = $(icon).attr("alt") == "Sticky Thread";
+
+                    let isToday = $(thread)
+                        .find("td:nth-of-type(3) > div")
+                        .text()
+                        .includes("Today");
+                    let replyCount = parseInt(
+                        $(thread)
+                        .find("td:nth-of-type(4) > a")
+                        .text()
+                        .replace(",", "")
+                    );
+                    let viewCount = parseInt(
+                        $(thread)
+                        .find("td:nth-of-type(5)")
+                        .text()
+                        .replace(",", "")
+                    );
+                    let news = {
+                        id: id,
+                        url: "https://forums.voz.vn/" + url,
+                        isToday: isToday,
+                        title: title,
+                        reply: replyCount,
+                        view: viewCount
+                    };
+                    if (!isSticky &&
+                        url.toLowerCase().includes("showthread") &&
+                        isToday
+                    ) {
+                        if (newsList.map(thread => thread.id).includes(news.id)) {
+                            newsList.splice(
+                                newsList.map(thread => thread.id).indexOf(news.id),
+                                1
+                            );
+                        }
+                        if (!posted.includes(news.id)) newsList.push(news);
+                    }
+                });
+                resolve(newsList);
+            }
+        );
+    });
 }
 
 function sortNewsList(newsList) {
-  newsList.sort(function(a, b) {
-    return b.reply * 0.7 + b.view * 0.3 - (a.reply * 0.7 + a.view * 0.3);
-  });
+    newsList.sort(function(a, b) {
+        return b.reply * 0.7 + b.view * 0.3 - (a.reply * 0.7 + a.view * 0.3);
+    });
 }
 
-module.exports = {
-  remindHolidayJob,
-  crawDataJob,
-  morningJob
+module.exports = function(bot) {
+    myBot = bot;
+    return { remindHolidayJob: remindHolidayJob, crawDataJob: crawDataJob, morningJob: morningJob };
 };
